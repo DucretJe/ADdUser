657c0537465b2b7c99e0b1abc00921a58af1e686
Kurdy34 <jdiphone34@gmail.com>
2019-05-05 18:50:40 +0200

    Fix bug in sender.py

--- sender.py
+++ sender.py
@@ -50,4 +50,3 @@ class Users():
     def SendAD(self,target):
         """ This method sends the powershell command to the AD server in order to add a user with the name, surname and title defined for the instance"""
         cmd = subprocess.Popen(["powershell",'Enter-PSSession {} ; New-ADUser -Name "{}" -Surname "{}" -SamAccountName "{}" -UserPrincipalName "{}" -DisplayName "{}" -ChangePasswordAtLogon 1 -Title "{}" -Path "OU=Users,OU={},OU=Services,DC={},DC={}" -AccountPassword(ConvertTo-SecureString p@ssw0rd% -AsPlainText -Force) -Enabled $true ; exit'.format(target, self.name,self.surname,self.SamAccountName,self.UserPrincipalName,self.DisplayName,self.title,self.service,self.dc1,self.dc2)])
-        cmd.terminate()
